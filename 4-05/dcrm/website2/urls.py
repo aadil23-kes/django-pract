ff9b9c6b411cf1464fae5ee6dce693db46161b79
aadil23-kes <aadilkeshwani4@gmail.com>
2025-05-11 14:09:03 +0530

    Add files via upload

--- 4-05/dcrm/website2/urls.py
+++ 4-05/dcrm/website2/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
-    path('all_emp',views.all_emp,name='all_emp')
+    path('all_emp',views.all_emp,name='all_emp'),
+    path('add_emp',views.add_emp,name='add_emp')
 
 ]
 
--- 4-05/dcrm/website2/views.py
+++ 4-05/dcrm/website2/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from website2.models import Employee
+from website2.models import Department, Employee, Role
 
 
 
@@ -19,3 +19,32 @@ def all_emp(request):
     }
     print(context)
     return render(request,'all_emp.html',context)
+
+
+def add_emp(request):
+   if request.method == 'POST':
+        print('post')
+        first_name = request.POST['first_name']
+        print(first_name)
+        last_name = request.POST['last_name']
+        salary = request.POST['salary']
+        bonus = request.POST['bonus']
+        phone = request.POST['phone']
+        hire_date = request.POST['hire_date']
+        print(last_name,salary,bonus,phone,hire_date)
+        dept = request.POST['dept']
+        role = request.POST['role']
+        
+        dept = Department.objects.get(id=dept)
+        role = Role.objects.get(id=role)
+
+        print(dept,role)
+
+
+ 
+
+
+   else:
+        print('get')
+
+   return render(request,'add_emp.html')
