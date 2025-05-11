d0b9e27fdadf39de3bd2ad03a15fc7c4dd078256
aadil23-kes <aadilkeshwani4@gmail.com>
2025-05-11 14:27:45 +0530

    Update views.py

--- 4-05/dcrm/website2/views.py
+++ 4-05/dcrm/website2/views.py
@@ -1,50 +1,66 @@
+from datetime import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
 
 from website2.models import Department, Employee, Role
 
-
-
-
 def index(request):
     #return HttpResponse("Hello from the blog!")
     return render(request,'index.html')
 
-
-
 def all_emp(request):
-    emps=Employee.objects.all()
-    context={
-        'emps':emps
+    emps = Employee.objects.all()
+    context = {
+        'emps': emps
     }
     print(context)
     return render(request,'all_emp.html',context)
 
 
 def add_emp(request):
-   if request.method == 'POST':
-        print('post')
+    if request.method == 'POST':
         first_name = request.POST['first_name']
-        print(first_name)
         last_name = request.POST['last_name']
         salary = request.POST['salary']
         bonus = request.POST['bonus']
         phone = request.POST['phone']
         hire_date = request.POST['hire_date']
-        print(last_name,salary,bonus,phone,hire_date)
-        dept = request.POST['dept']
-        role = request.POST['role']
-        
-        dept = Department.objects.get(id=dept)
-        role = Role.objects.get(id=role)
+        dept_id = request.POST['dept']
+        role_id = request.POST['role']
 
-        print(dept,role)
+        # Get related objects from DB
+        dept = Department.objects.get(id=dept_id)
+        role = Role.objects.get(id=role_id)
+
+        # Save employee
+        emp = Employee(
+            first_name=first_name,
+            last_name=last_name,
+            salary=salary,
+            bonus=bonus,
+            phone=phone,
+            hire_date=hire_date,
+            dept=dept,
+            role=role
+        )
+        emp.save()
+
+        # After saving, return a response with the updated list of departments and roles
+        departments = Department.objects.all()
+        roles = Role.objects.all()
+        return render(request, 'add_emp.html', {'departments': departments, 'roles': roles, 'message': "Employee added successfully!"})
+
+    elif request.method == 'GET':
+        departments = Department.objects.all()
+        roles = Role.objects.all()
+        return render(request, 'add_emp.html', {'departments': departments, 'roles': roles})
+    
 
 
- 
+
+def remove_emp(request):
+    return render(request,'remove_emp.html')
 
 
-   else:
-        print('get')
-
-   return render(request,'add_emp.html')
+def filter_emp(request):
+    return render(request,'filter_emp.html')
